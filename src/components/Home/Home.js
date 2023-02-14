780aa159bc9ff8d09ebb395590ab78fc4efb5fdd
yoon <parkyoon97@gmail.com>
2023-02-14 13:12:54 +0000

    finished

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import classes from "./Home.module.css";
+import AuthContext from '../../store/auth-context';
 
 const Home = (props) => {
   const authCtx = useContext(AuthContext);
